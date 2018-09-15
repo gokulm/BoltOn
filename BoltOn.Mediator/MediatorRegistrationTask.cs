092e0b046dbeff5abb73e01341c8da28df7a5e3d
gokulm <gokulnathmurugesan@gmail.com>
2018-09-15 17:04:47 -0400

    added handler registration task

---
+++ BoltOn.Mediator/MediatorRegistrationTask.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using BoltOn.Bootstrapping;
+using BoltOn.IoC;
+
+namespace BoltOn.Mediator
+{
+	public class MediatorRegistrationTask : IBootstrapperRegistrationTask
+	{
+		public void Run(IBoltOnContainer container, IEnumerable<Assembly> assemblies)
+		{
+			container.RegisterScoped<IMediator, Mediator>();
+			//container.RegisterTransient<Mediator>();
+			RegisterHandlers(container, assemblies);
+		}
+
+		private void RegisterHandlers(IBoltOnContainer container, IEnumerable<Assembly> assemblies)
+		{
+			var requestHandlerInterfaceType = typeof(IRequestHandler<,>);
+			var handlers = (from a in assemblies.ToList()
+							from t in a.GetTypes()
+							from i in t.GetInterfaces()
+							where i.IsGenericType &&
+								requestHandlerInterfaceType.IsAssignableFrom(i.GetGenericTypeDefinition())
+							select new { Interface = i, Implementation = t }).ToList();
+
+			foreach (var handler in handlers)
+			{
+				container.RegisterTransient(handler.Interface, handler.Implementation);
+			}
+		}
+	}
+}
--- BoltOn.Tests/Mediator/MediatorTests.cs
+++ BoltOn.Tests/Mediator/MediatorTests.cs
@@ -2,6 +2,8 @@
 using BoltOn.Bootstrapping;
 using Xunit;
 using BoltOn.Mediator;
+using BoltOn.IoC;
+using BoltOn.IoC.SimpleInjector;
 
 namespace BoltOn.Tests.Mediator
 {
@@ -10,13 +12,15 @@ namespace BoltOn.Tests.Mediator
 		[Fact]
 		public void GetInstance_RegisterTransient_ResolvesDependencies()
 		{
-
 			// arrange
 			Bootstrapper
 				.Instance
+				.ExcludeAssemblies(typeof(SimpleInjectorContainerAdapter).Assembly)
 				.Run();
 
 			// act
+			var mediator = ServiceLocator.Current.GetInstance<BoltOn.Mediator.IMediator>();
+			mediator.Get(new TestRequest());
 
 			// assert
 		}
