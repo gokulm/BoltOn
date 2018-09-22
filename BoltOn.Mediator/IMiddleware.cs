6e1755f431aeded75ef3df0ee324fc9e3cc491f4
gokulm <gokulnathmurugesan@gmail.com>
2018-09-22 19:37:34 -0400

    added middleware implementation

--- BoltOn.Mediator/IMiddleware.cs
+++ BoltOn.Mediator/IMiddleware.cs
@@ -1,9 +1,31 @@
+using System;
+using BoltOn.Logging;
+
 namespace BoltOn.Mediator
 {
-    public delegate TResponse HandleDelegate<in TRequest, TResponse>(TRequest request);
+    // public delegate TResponse Handle<in TRequest, TResponse>(TRequest request);
 
     public interface IMiddleware<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
-        TResponse RunAsync(TRequest request, HandleDelegate<TRequest, TResponse> next);
+        StandardDtoReponse<TResponse> Run(IRequest<TResponse> request, Func<IRequest<TResponse>, StandardDtoReponse<TResponse>> next);
+    }
+
+    public class PerformanceMiddleware<TRequest, TResponse> : IMiddleware<TRequest, TResponse> 
+    where TRequest : IRequest<TResponse>
+    {
+        private readonly IBoltOnLogger<PerformanceMiddleware<TRequest, TResponse>> _logger;
+
+        public PerformanceMiddleware(IBoltOnLogger<PerformanceMiddleware<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public StandardDtoReponse<TResponse> Run(IRequest<TResponse> request, Func<IRequest<TResponse>, StandardDtoReponse<TResponse>> next)
+        {
+            _logger.Debug("started");
+            var response = next.Invoke(request);
+            _logger.Debug("completed");
+            return response;
+        }
     }
 }
--- BoltOn.Mediator/Mediator.cs
+++ BoltOn.Mediator/Mediator.cs
@@ -1,56 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using BoltOn.Logging;
 using BoltOn.Utilities;
 
 namespace BoltOn.Mediator
 {
-	public interface IMediator
-	{
-		StandardDtoReponse<TResponse> Get<TResponse>(IRequest<TResponse> request);
-	}
+    public interface IMediator
+    {
+        StandardDtoReponse<TResponse> Get<TResponse>(IRequest<TResponse> request);
+    }
 
-	public class Mediator : IMediator
-	{
-		private readonly IoC.IServiceFactory _serviceFactory;
-		private readonly IBoltOnLogger<Mediator> _logger;
+    public class Mediator : IMediator
+    {
+        private readonly IoC.IServiceFactory _serviceFactory;
+        private readonly IBoltOnLogger<Mediator> _logger;
 
-		public Mediator(IoC.IServiceFactory serviceProvider, IBoltOnLogger<Mediator> logger)
-		{
-			_logger = logger;
-			_serviceFactory = serviceProvider;
-		}
+        public Mediator(IoC.IServiceFactory serviceFactory, IBoltOnLogger<Mediator> logger)
+        {
+            _logger = logger;
+            _serviceFactory = serviceFactory;
+        }
 
-		public StandardDtoReponse<TResponse> Get<TResponse>(IRequest<TResponse> request)
-		{
-			try
-			{
-				var requestType = request.GetType();
-				_logger.Debug($"Resolving handler for request: {requestType}");
-				var genericRequestHandlerType = typeof(IRequestHandler<,>);
-				var interfaceHandlerType =
-					genericRequestHandlerType.MakeGenericType(request.GetType(), typeof(TResponse));
-				dynamic handler = _serviceFactory.GetInstance(interfaceHandlerType);
-				Check.Requires(handler != null, string.Format(Constants.ExceptionMessages.HANDLER_NOT_FOUND, requestType));
-				_logger.Debug($"Resolved handler: {handler.GetType()}");
-				var response = handler.Handle(request);
-				return new StandardDtoReponse<TResponse> 
-				{ 
-					Data = response, 
-					IsSuccessful = true 
-				};
-			}
-			catch (Exception ex)
-			{
-				_logger.Error(ex);
-				return new StandardDtoReponse<TResponse>
-				{
-					IsSuccessful = false,
-					Exception = ex
-				};
-			}
-			finally
-			{
-			}
-		}
-	}
+        public StandardDtoReponse<TResponse> Handle<TResponse>(IRequest<TResponse> request)
+        {
+			return RunMiddleware<TResponse>(request, Get);
+        }
+
+		private StandardDtoReponse<TResponse> RunMiddleware<TResponse>(IRequest<TResponse> request, 
+			Func<IRequest<TResponse>, StandardDtoReponse<TResponse>> handleRequestHandlerCall) 
+        {
+            Func<IRequest<TResponse>, StandardDtoReponse<TResponse>> next = null;
+			var middlewares = (IEnumerable<IMiddleware<IRequest<TResponse>, TResponse>>)
+                _serviceFactory.GetInstance(typeof(IEnumerable<IMiddleware<IRequest<TResponse>, TResponse>>));
+
+            next = middlewares.Reverse().Aggregate(handleRequestHandlerCall, (requestDelegate, middleware) =>
+                ((req) => middleware.Run(req, requestDelegate)));
+
+            return next.Invoke(request);
+        }
+
+        public StandardDtoReponse<TResponse> Get<TResponse>(IRequest<TResponse> request)
+        {
+            try
+            {
+                var requestType = request.GetType();
+                _logger.Debug($"Resolving handler for request: {requestType}");
+                var genericRequestHandlerType = typeof(IRequestHandler<,>);
+                var interfaceHandlerType =
+                    genericRequestHandlerType.MakeGenericType(request.GetType(), typeof(TResponse));
+                dynamic handler = _serviceFactory.GetInstance(interfaceHandlerType);
+                Check.Requires(handler != null, string.Format(Constants.ExceptionMessages.HANDLER_NOT_FOUND, requestType));
+                _logger.Debug($"Resolved handler: {handler.GetType()}");
+                var response = handler.Handle(request);
+                return new StandardDtoReponse<TResponse>
+                {
+                    Data = response,
+                    IsSuccessful = true
+                };
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex);
+                return new StandardDtoReponse<TResponse>
+                {
+                    IsSuccessful = false,
+                    Exception = ex
+                };
+            }
+            finally
+            {
+            }
+        }
+    }
 }
