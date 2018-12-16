567776a99247bdb0caeba5b8b35754219a495587
gokulm <gokulnathmurugesan@gmail.com>
2018-12-15 19:04:41 -0500

    added samples projs under samples folder

---
+++ samples/BoltOn.Samples.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using BoltOn.Mediator.Pipeline;
+using BoltOn.Samples.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoltOn.Samples.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public ValuesController(IMediator mediator)
+		{
+			this._mediator = mediator;
+		}
+
+		// GET api/values
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			var response = _mediator.Get(new TestRequest());
+			return new string[] { response.Data.Test };
+		}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/values
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
