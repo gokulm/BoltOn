c71a09eb18810612f237e6022975be8e7d631fb1
gokulm <gokulnathmurugesan@gmail.com>
2019-04-06 14:58:28 -0400

    updated csproj file for nuget. updated assemblyinfo

--- src/BoltOn/AssemblyInfo.cs
+++ src/BoltOn/AssemblyInfo.cs
@@ -1,4 +1,11 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Reflection;
 
 [assembly: InternalsVisibleTo("BoltOn.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+[assembly: AssemblyCompany("BoltOn")]
+[assembly: AssemblyProduct("BoltOn")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-beta")]
