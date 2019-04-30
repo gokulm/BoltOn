96c1442b18c9224f18dd0a7d9c2212509c623a3a
gokulm <gokulnathmurugesan@gmail.com>
2019-04-29 22:20:03 -0400

    added handler template

---
+++ cli/templates/BoltOn.HandlerTemplate/content/Handlers/BoltOnHandlerHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BoltOn;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BoltOn.Handler
+{
+    public class BoltOnHandlerRequest : IQuery<IEnumerable<StudentDto>>
+	{
+	}
+
+	public class BoltOnHandlerHandler : IRequestAsyncHandler<GetAllStudentsRequest, IEnumerable<StudentDto>>
+	{
+		private readonly IStudentRepository _studentRepository;
+
+		public BoltOnHandlerHandler(IStudentRepository studentRepository)
+		{
+			_studentRepository = studentRepository;
+		}
+
+		public async Task<IEnumerable<StudentDto>> HandleAsync(GetAllStudentsRequest request, CancellationToken cancellationToken = default(CancellationToken))
+		{
+			var students = (await _studentRepository.GetAllAsync()).ToList();
+			var studentDtos = from s in students
+							   select new StudentDto
+							   {
+								   FirstName = s.FirstName,
+								   LastName = s.LastName
+							   };
+			return studentDtos;
+		}
+	}
+}
