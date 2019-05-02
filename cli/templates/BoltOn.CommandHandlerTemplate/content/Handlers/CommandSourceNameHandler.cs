fcbacb470a038b19bb4fd896c499628c011cb17d
gokulm <gokulnathmurugesan@gmail.com>
2019-05-01 21:43:12 -0400

    added query and command handler templates

---
+++ cli/templates/BoltOn.CommandHandlerTemplate/content/Handlers/CommandSourceNameHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BoltOn;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BoltOn.Handler
+{
+    public class CommandSourceNameRequest : ICommand<ResponseType>
+	{
+	}
+
+	public class CommandSourceNameHandler : IRequestAsyncHandler<CommandSourceNameRequest, ResponseType>
+	{
+		public async Task<ResponseType> HandleAsync(CommandSourceNameRequest request, 
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			return default(ResponseType);
+		}
+	}
+}
---
+++ cli/templates/BoltOn.QueryHandlerTemplate/content/Handlers/QuerySourceNameHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BoltOn;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BoltOn.Handler
+{
+    public class QuerySourceNameRequest : IQuery<ResponseType>
+	{
+	}
+
+	public class QuerySourceNameHandler : IRequestAsyncHandler<QuerySourceNameRequest, ResponseType>
+	{
+		public async Task<ResponseType> HandleAsync(QuerySourceNameRequest request, 
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			return default(ResponseType);
+		}
+	}
+}
