1dbd41b46938936a0bb07a8b2a40ea35e67a51c4
gokulm <gokulnathmurugesan@gmail.com>
2019-05-05 14:33:26 -0400

    renamed templates

---
+++ cli/templates/BoltOn.QueryHandlerTemplate/content/Handlers/QueryHandlerTemplateHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BoltOn;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BoltOn.Handler
+{
+    public class QueryHandlerTemplateRequest : IQuery<ResponseType>
+	{
+	}
+
+	public class QueryHandlerTemplateHandler : IRequestAsyncHandler<QueryHandlerTemplateRequest, ResponseType>
+	{
+		public async Task<ResponseType> HandleAsync(QueryHandlerTemplateRequest request, 
+			CancellationToken cancellationToken = default(CancellationToken))
+		{
+			return default(ResponseType);
+		}
+	}
+}
