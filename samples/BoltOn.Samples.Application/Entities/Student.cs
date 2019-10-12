5b15d6924ab515dafb3c5402592afd6a38313296
gokulm <gokulnathmurugesan@gmail.com>
2019-10-12 07:34:46 -0400

    added studentqueryentity

--- samples/BoltOn.Samples.Application/Entities/Student.cs
+++ samples/BoltOn.Samples.Application/Entities/Student.cs
@@ -1,10 +1,44 @@
-using BoltOn.Data;
+using BoltOn.Cqrs;
+using BoltOn.Samples.Application.Handlers;
 
 namespace BoltOn.Samples.Application.Entities
 {
-	public class Student : BaseEntity<int>
+	public class Student : BaseCqrsEntity
 	{
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
+
+		private Student()
+		{
+		}
+
+		public Student(int id, string firstName, string lastName)
+		{
+			Id = id.ToString();
+			FirstName = firstName;
+			LastName = lastName;
+
+			RaiseEvent(new StudentCreatedEvent { Student = this });
+		}
+	}
+
+	public class StudentQueryEntity : BaseCqrsEntity
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+		private StudentQueryEntity()
+		{
+		}
+
+		public StudentQueryEntity(StudentCreatedEvent @event)
+		{
+			ProcessEvent(@event, e =>
+			{
+				Id = e.Student.Id;
+				FirstName = e.Student.FirstName;
+				LastName = e.Student.LastName;
+			});
+		}
 	}
 }
--- samples/BoltOn.Samples.Application/Handlers/CreateStudentHandler.cs
+++ samples/BoltOn.Samples.Application/Handlers/CreateStudentHandler.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using BoltOn.Cqrs;
+using BoltOn.Data;
 using BoltOn.Mediator.Pipeline;
+using BoltOn.Samples.Application.Entities;
 
 namespace BoltOn.Samples.Application.Handlers
 {
@@ -12,11 +16,44 @@ namespace BoltOn.Samples.Application.Handlers
 	}
 
 	public class CreateStudentHandler : IRequestAsyncHandler<CreateStudentRequest>
-    {
-        public async Task HandleAsync(CreateStudentRequest request, CancellationToken cancellationToken)
-        {
-            System.Console.WriteLine("message from CreateStudentHandler: " + request.FirstName);
-            await Task.FromResult("testing");
-        }
-    }
+	{
+		private readonly IRepository<Student> _studentRepository;
+
+		public CreateStudentHandler(IRepository<Student> studentRepository)
+		{
+			_studentRepository = studentRepository;
+		}
+
+		public async Task HandleAsync(CreateStudentRequest request, CancellationToken cancellationToken)
+		{
+			await _studentRepository.AddAsync(
+				new Student
+				(
+					1,
+					request.FirstName,
+					request.LastName
+				), cancellationToken);
+			System.Console.WriteLine("message from CreateStudentHandler: " + request.FirstName);
+		}
+	}
+
+	public class StudentCreatedEvent : EventToBeProcessed
+	{
+		public Student Student { get; set; }
+	}
+
+	public class StudentCreatedEventHandler : IRequestAsyncHandler<StudentCreatedEvent>
+	{
+		private readonly IRepository<StudentQueryEntity> _repository;
+
+		public StudentCreatedEventHandler(IRepository<StudentQueryEntity> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task HandleAsync(StudentCreatedEvent request, CancellationToken cancellationToken)
+		{
+			await _repository.AddAsync(new StudentQueryEntity(request), cancellationToken);
+		}
+	}
 }
