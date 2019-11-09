d838413f523f15b6717954ad80c5892688f03787
gokulm <gokulnathmurugesan@gmail.com>
2019-11-09 14:28:55 -0500

    merged master to develop

--- src/BoltOn.Data.EF/AssemblyInfo.cs
+++ src/BoltOn.Data.EF/AssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("BoltOn")]
 [assembly: AssemblyProduct("BoltOn.Data.EF")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
-[assembly: AssemblyInformationalVersion("0.2.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyInformationalVersion("0.3.0")]
