debea35248692a981e6b0b7c4ba1079a55b31cc2
Gokulnath Murugesan <gokulnathmurugesan@gmail.com>
2019-11-10 11:38:59 -0500

    Revert "Fixed UoW (#41)"
    This reverts commit 661db34f749cd60d62941fecf358a6410d23f7d1.

--- src/BoltOn/Bootstrapping/BoltOnRegistrationTask.cs
+++ src/BoltOn/Bootstrapping/BoltOnRegistrationTask.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using BoltOn.Cqrs;
+using BoltOn.Data;
 using BoltOn.Logging;
 using BoltOn.Mediator.Interceptors;
 using BoltOn.Mediator.Pipeline;
@@ -44,6 +45,7 @@ namespace BoltOn.Bootstrapping
 		private void RegisterOtherTypes(RegistrationTaskContext context)
 		{
 			var serviceCollection = context.Container;
+			serviceCollection.AddScoped<IUnitOfWorkManager, UnitOfWorkManager>();
 			serviceCollection.AddScoped<IUnitOfWorkManager>(s =>
 			{
 				return new UnitOfWorkManager(s.GetRequiredService<IBoltOnLogger<UnitOfWorkManager>>(), 
