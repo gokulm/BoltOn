2d18e68af332cce40da19ed30892f59cc3762be6
gokulm <gokulnathmurugesan@gmail.com>
2019-11-17 10:18:39 -0500

    changed versions

--- src/BoltOn.Data.EF/AssemblyInfo.cs
+++ src/BoltOn.Data.EF/AssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("BoltOn")]
 [assembly: AssemblyProduct("BoltOn.Data.EF")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
-[assembly: AssemblyInformationalVersion("0.3.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyInformationalVersion("0.4.0")]
