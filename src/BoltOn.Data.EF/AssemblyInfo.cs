d03fd712c90646f255557754479db9b2c31bdb3c
Gokulnath Murugesan <gokulnathmurugesan@gmail.com>
2019-12-16 08:16:13 -0500

    Master to Develop (#53)
    * Develop to Master (#50)
    
    * CQRS New Options and Serialization Fix (#52)
    
    Fixed CQRS serialization issue
    Added ClearEventsEnabled flag
    Changed CQRS EF entity mapping to serialize ProcessedEvents and EventsToBeProcessed as string
    Refactored IInterceptor method signature to support unit testing
    Added unit and integration tests for CQRS components, especially CqrsInterceptor
    Moved fakes (mocks and stubs) to a separate folder

--- src/BoltOn.Data.EF/AssemblyInfo.cs
+++ src/BoltOn.Data.EF/AssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("BoltOn")]
 [assembly: AssemblyProduct("BoltOn.Data.EF")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
-[assembly: AssemblyInformationalVersion("0.4.0")]
+[assembly: AssemblyVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
+[assembly: AssemblyInformationalVersion("0.4.1")]
