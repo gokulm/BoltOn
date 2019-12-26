1301d860b126928b63a70be91757f859c4a71799
gokulm <gokulnathmurugesan@gmail.com>
2019-12-26 10:03:25 -0500

    included changedfiles and commits

--- build/build.ps1
+++ build/build.ps1
@@ -3,11 +3,20 @@ Param([string]$branchName)
 $_branchName = $branchName
 $_scriptDirPath = $PSScriptRoot
 $_boltOnModulePath = Join-Path $_scriptDirPath "bolton.psm1"
+$_changedFiles = ''
 
 function Main
 {
     Import-Module $_boltOnModulePath -Force
     LogDebug "Branch: $_branchName"
+    if ($_branchName)  {
+        $global:_changedFiles = git diff "origin/$_branchName...HEAD" --no-commit-id --name-only
+        LogInfo "Files Changed:" $_changedFiles
+
+        $commits = git log -n 5
+        LogInfo "Files Changed:" $_changedFiles
+    }
+
     BuildAndTest
 }
 
