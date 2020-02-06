ae9e340136fa6df4baa20c1c279da0487c899c19
gokulm <gmurugesan@evolenthealth.com>
2020-02-06 11:48:30 -0500

    removed bolton.overrides dependency from tests

---
+++ tests/BoltOn.Tests/Mediator/Fakes/CustomChangeTrackerInterceptor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BoltOn.Data.EF;
+using BoltOn.Logging;
+using BoltOn.Mediator.Interceptors;
+using BoltOn.Mediator.Pipeline;
+
+namespace BoltOn.Tests.Mediator.Fakes
+{
+	public class CustomChangeTrackerInterceptor : IInterceptor
+	{
+		private readonly IBoltOnLogger<CustomChangeTrackerInterceptor> _logger;
+		private readonly ChangeTrackerContext _changeTrackerContext;
+
+		public CustomChangeTrackerInterceptor(IBoltOnLogger<CustomChangeTrackerInterceptor> logger,
+			ChangeTrackerContext changeTrackerContext)
+		{
+			_logger = logger;
+			_changeTrackerContext = changeTrackerContext;
+		}
+
+		public async Task<TResponse> RunAsync<TRequest, TResponse>(TRequest request, CancellationToken cancellationToken, 
+			Func<TRequest, CancellationToken, Task<TResponse>> next) where TRequest : IRequest<TResponse>
+		{
+			_logger.Debug($"Entering {nameof(CustomChangeTrackerInterceptor)}...");
+			_changeTrackerContext.IsQueryRequest = request is IQuery<TResponse> || request is IQueryUncommitted<TResponse>;
+			_logger.Debug($"IsQueryRequest or IQueryUncommitted: {_changeTrackerContext.IsQueryRequest}");
+			var response = await next(request, cancellationToken);
+			return response;
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
