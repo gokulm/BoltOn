cee6d2b84d3425984373dcf98339d253cc9dfa0d
gokulm <gmurugesan@evolenthealth.com>
2020-03-17 16:51:56 -0400

    added appsettings to samples tests

--- samples/upgrade-samples.ps1
+++ samples/upgrade-samples.ps1
@@ -12,13 +12,22 @@ function Main {
         Import-Module $_boltOnModulePath -Force
         LogBeginFunction "$($MyInvocation.MyCommand.Name)"
         dotnet tool install nukeeper --global
+
+        nukeeper update --include=^BoltOn --source=https://api.nuget.org/v3/index.json
+        docker-compose up -d --build
+        Start-Sleep -s 10
+
+        $appSettingsPath = Join-Path $_scriptDirPath "BoltOn.Sample.WebApi"
+        $appSettingsPath = Join-Path $appSettingsPath "appsettings.json"
+        $appSettingsFile = Get-Content $appSettingsPath -raw | ConvertFrom-Json
+        $appSettingsFile.update $_.IsIntegrationTestsEnabled="true"
+        $appSettingsFile | ConvertTo-Json -depth 32| set-content $appSettingsPath
+
+        Test
+
         if ($_branchName -eq "master") {
             nukeeper repo "https://github.com/gokulm/BoltOn/" $GITHUB_TOKEN --fork=SingleRepositoryOnly `
                 --source=https://api.nuget.org/v3/index.json --include=^BoltOn --consolidate --targetBranch=master
-        }
-        else {
-            nukeeper update --include=^BoltOn --source=https://api.nuget.org/v3/index.json
-            docker-compose up -d --build
         }
         LogEndFunction "$($MyInvocation.MyCommand.Name)"
     }
