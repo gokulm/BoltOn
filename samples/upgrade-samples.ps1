284ee84ef6254301a603a094f9980c3ffcb500b4
gokulm <gmurugesan@evolenthealth.com>
2020-03-17 18:01:31 -0400

    refactored upgrade-samples.ps1 to separate fns

--- samples/upgrade-samples.ps1
+++ samples/upgrade-samples.ps1
@@ -15,25 +15,13 @@ function Main {
         Import-Module $_boltOnModulePath -Force
         LogBeginFunction "$($MyInvocation.MyCommand.Name)"
 
-        dotnet tool install nukeeper --global
-        nukeeper update --include=^BoltOn --source=https://api.nuget.org/v3/index.json
-        docker-compose down 
-        docker-compose up -d --build
-        Start-Sleep -s 60
-
-        $appSettingsPath = Join-Path $_scriptDirPath "BoltOn.Samples.Tests"
-        $appSettingsPath = Join-Path $appSettingsPath "appsettings.json"
-        $appSettingsFile = Get-Content $appSettingsPath -raw | ConvertFrom-Json
-        $appSettingsFile.IsIntegrationTestsEnabled=$true
-        $appSettingsFile | ConvertTo-Json | Set-Content $appSettingsPath
-
+        UpgradeBoltOnNuGetPackages
+        DockerCompose
+        EnableIntegrationTests
         Build
         Test
-
-        if ($_branchName -eq "master") {
-            nukeeper repo "https://github.com/gokulm/BoltOn/" $GITHUB_TOKEN --fork=SingleRepositoryOnly `
-                --source=https://api.nuget.org/v3/index.json --include=^BoltOn --consolidate --targetBranch=master
-        }
+        UpgradeSamplesAndCreatePR
+     
         LogEndFunction "$($MyInvocation.MyCommand.Name)"
     }
     catch {
@@ -42,6 +30,44 @@ function Main {
             exit $LASTEXITCODE
         }
     }
+}
+
+function UpgradeBoltOnNuGetPackages
+{
+    LogBeginFunction "$($MyInvocation.MyCommand.Name)"
+    dotnet tool install nukeeper --global
+    nukeeper update --include=^BoltOn --source=https://api.nuget.org/v3/index.json --age 0
+    LogEndFunction "$($MyInvocation.MyCommand.Name)"
+}
+
+function DockerCompose
+{
+    LogBeginFunction "$($MyInvocation.MyCommand.Name)"
+    docker-compose down 
+    docker-compose up -d --build
+    Start-Sleep -s 60
+    LogEndFunction "$($MyInvocation.MyCommand.Name)"
+}
+
+function EnableIntegrationTests
+{
+    LogBeginFunction "$($MyInvocation.MyCommand.Name)"
+    $appSettingsPath = Join-Path $_scriptDirPath "BoltOn.Samples.Tests"
+    $appSettingsPath = Join-Path $appSettingsPath "appsettings.json"
+    $appSettingsFile = Get-Content $appSettingsPath -raw | ConvertFrom-Json
+    $appSettingsFile.IsIntegrationTestsEnabled=$true
+    $appSettingsFile | ConvertTo-Json | Set-Content $appSettingsPath
+    LogEndFunction "$($MyInvocation.MyCommand.Name)"
+}
+
+function UpgradeSamplesAndCreatePR
+{
+    LogBeginFunction "$($MyInvocation.MyCommand.Name)"
+    if ($_branchName -eq "master") {
+        nukeeper repo "https://github.com/gokulm/BoltOn/" $GITHUB_TOKEN --fork=SingleRepositoryOnly `
+            --source=https://api.nuget.org/v3/index.json --include=^BoltOn --consolidate --targetBranch=master
+    }
+    LogEndFunction "$($MyInvocation.MyCommand.Name)"
 }
 
 Main
