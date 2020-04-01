40020c3ed7a02932f5e6416ea08b821c7398733f
gokulm <gokulnathmurugesan@gmail.com>
2020-04-01 11:22:01 -0400

    fixed integ tests

--- samples/BoltOn.Samples.WebApi/Controllers/StudentsController.cs
+++ samples/BoltOn.Samples.WebApi/Controllers/StudentsController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using BoltOn.Requestor.Pipeline;
@@ -26,13 +25,13 @@ namespace BoltOn.Samples.WebApi.Controllers
 		}
 
 		[HttpPost, Route("[controller]")]
-		public async Task<Student> Post(CreateStudentRequest request)
+		public async Task<Student> Post([FromBody]CreateStudentRequest request)
 		{
 			return await _requestor.ProcessAsync(request);
 		}
 
 		[HttpPut, Route("[controller]")]
-		public async Task<string> Put(UpdateStudentRequest request)
+		public async Task<string> Put([FromBody]UpdateStudentRequest request)
 		{
 			await _requestor.ProcessAsync(request);
 			return "Updated";
