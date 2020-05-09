68e6ee519c3595ad56fdd1c41b5e577809482a4b
gokulm <gokulnathmurugesan@gmail.com>
2020-05-09 10:20:16 -0400

    added old and temp folder

---
+++ src/old/BoltOnCacheSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace BoltOn.Cache
+{
+    public interface IBoltOnCacheSerializer
+    {
+        byte[] ToByteArray(object obj);
+        T FromByteArray<T>(byte[] byteArray) where T : class;
+    }
+
+    public class BoltOnCacheSerializer : IBoltOnCacheSerializer
+    {
+        public T FromByteArray<T>(byte[] byteArray) where T : class
+        {
+            if (byteArray == default || byteArray.Length == 0)
+                return default;
+
+            return JsonSerializer.Deserialize<T>(byteArray);
+        }
+
+        public byte[] ToByteArray(object obj)
+        {
+            return JsonSerializer.SerializeToUtf8Bytes(obj);
+        }
+    }
+}
---
+++ src/old/Extensions.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using BoltOn.Bootstrapping;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BoltOn.Cache
+{
+	public static class Extensions
+	{
+		public static BoltOnOptions BoltOnCache(this BoltOnOptions boltOnOptions)
+		{
+			boltOnOptions.BoltOnAssemblies(Assembly.GetExecutingAssembly());
+			boltOnOptions.ServiceCollection.AddTransient<IBoltOnCache, BoltOnCache>();
+			return boltOnOptions;
+		}
+	}
+}
