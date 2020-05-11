a23e2c36a0c433ecdb66a07cae59fb0b28c95c56
gokulm <gokulnathmurugesan@gmail.com>
2020-05-11 07:03:40 -0400

    added cache to samples

--- samples/BoltOn.Samples.Application/Handlers/GetAllStudentsHandler.cs
+++ samples/BoltOn.Samples.Application/Handlers/GetAllStudentsHandler.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using BoltOn.Cache;
 using BoltOn.Data;
 using BoltOn.Requestor.Pipeline;
 using BoltOn.Samples.Application.DTOs;
@@ -16,24 +17,35 @@ namespace BoltOn.Samples.Application.Handlers
     public class GetAllStudentsHandler : IHandler<GetAllStudentsRequest, IEnumerable<StudentDto>>
     {
         private readonly IRepository<StudentFlattened> _studentRepository;
+		private readonly IBoltOnCache _boltOnCache;
 
-        public GetAllStudentsHandler(IRepository<StudentFlattened> studentRepository)
+		public GetAllStudentsHandler(IRepository<StudentFlattened> studentRepository,
+			IBoltOnCache boltOnCache)
         {
             _studentRepository = studentRepository;
-        }
+			_boltOnCache = boltOnCache;
+		}
         
 		public async Task<IEnumerable<StudentDto>> HandleAsync(GetAllStudentsRequest request, 
 			CancellationToken cancellationToken = default)
 		{
-			var students = (await _studentRepository.GetAllAsync(cancellationToken: cancellationToken)).ToList();
-			var studentDtos = from s in students
+			var students = await _boltOnCache.GetAsync("Students", cancellationToken,
+				valueGetter: async () => await GetStudents(cancellationToken));
+			return students;
+		}
+
+		private async Task<IEnumerable<StudentDto>> GetStudents(CancellationToken cancellationToken = default)
+		{
+			var temp = (await _studentRepository.GetAllAsync(cancellationToken: cancellationToken)).ToList();
+			var studentDtos = (from s in temp
 							   select new StudentDto
 							   {
 								   Id = s.Id,
 								   FirstName = s.FirstName,
 								   LastName = s.LastName,
 								   StudentType = s.StudentType
-							   };
+							   }).ToList();
+
 			return studentDtos;
 		}
 	}
