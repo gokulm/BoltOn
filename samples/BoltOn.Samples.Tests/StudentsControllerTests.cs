b6fc0987d463ddf929e2a016a488636e949b2a6e
gokulm <gokulnathmurugesan@gmail.com>
2020-05-12 06:28:13 -0400

    updated docs

--- samples/BoltOn.Samples.Tests/StudentsControllerTests.cs
+++ samples/BoltOn.Samples.Tests/StudentsControllerTests.cs
@@ -4,7 +4,6 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Xunit;
-using BoltOn.Samples.Application.Entities;
 using BoltOn.Samples.Application.Handlers;
 using System.Collections.Generic;
 using System.Linq;
