3243864e95fcb8a519fd955b20d748e2b82bb6c5
gokulm <gokulnathmurugesan@gmail.com>
2021-04-22 06:27:52 -0400

    updated hangfireprocessor

--- src/BoltOn.Hangfire/AppHangfireJobProcessor.cs
+++ src/BoltOn.Hangfire/AppHangfireJobProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using BoltOn.Logging;
 using BoltOn.Requestor.Pipeline;
 using Hangfire;
 
@@ -8,17 +9,22 @@ namespace BoltOn.Hangfire
 	public class AppHangfireJobProcessor
 	{
 		private readonly IRequestor _requestor;
+		private readonly IAppLogger<AppHangfireJobProcessor> _logger;
 
-		public AppHangfireJobProcessor(IRequestor requestor)
+		public AppHangfireJobProcessor(IRequestor requestor,
+			IAppLogger<AppHangfireJobProcessor> logger)
 		{
 			_requestor = requestor;
+			_logger = logger;
 		}
 
 		[JobDisplayName("{0}")]
 		public async Task ProcessAsync<TRequest>(TRequest request, CancellationToken cancellationToken = default)
 			where TRequest : IRequest
 		{
+			_logger.Debug($"Sending request of type {request.GetType().Name} to requestor...");
 			await _requestor.ProcessAsync(request, cancellationToken);
+			_logger.Debug("Request sent to Requestor");
 		}
 	}
 }
