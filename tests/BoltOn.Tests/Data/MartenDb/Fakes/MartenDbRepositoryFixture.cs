f0764d90920e11a2a4aefe179b9b529a69263c4e
gokulm <gokulnathmurugesan@gmail.com>
2021-12-21 06:35:11 -0500

    fixed martendb tests

--- tests/BoltOn.Tests/Data/MartenDb/Fakes/MartenDbRepositoryFixture.cs
+++ tests/BoltOn.Tests/Data/MartenDb/Fakes/MartenDbRepositoryFixture.cs
@@ -48,7 +48,7 @@ namespace BoltOn.Tests.Data.MartenDb.Fakes
 		{
 			var options = new StoreOptions();
 			options.Connection("host=localhost;database=bolton;password=bolton;username=bolton");
-			options.AutoCreateSchemaObjects = AutoCreate.All;
+			options.AutoCreateSchemaObjects = Weasel.Postgresql.AutoCreate.All;
 
 			return options;
 		}
@@ -95,7 +95,7 @@ namespace BoltOn.Tests.Data.MartenDb.Fakes
 		{
 			var options = new StoreOptions();
 			options.Connection("host=localhost;database=bolton;password=bolton;username=bolton");
-			options.AutoCreateSchemaObjects = AutoCreate.All;
+			options.AutoCreateSchemaObjects = Weasel.Postgresql.AutoCreate.All;
 
 			return options;
 		}
