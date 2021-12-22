c1c9b1dda9806acbaf7c6c65a9036d8e06a1e22f
Gokulnath Murugesan <gokulnathmurugesan@gmail.com>
2021-12-22 06:42:42 -0500

    feat(BoltOn,BoltOn.Bus.MassTransit,BoltOn.Cache,BoltOn.Data.EF,BoltOn.Data.Elasticsearch,BoltOn.Data.MartenDb,BoltOn.Hangfire,BoltOn.Web)
    * upgraded versions of all the projects

--- src/BoltOn.Data.CosmosDb/BaseCosmosDbOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BoltOn.Data.CosmosDb
-{
-	public abstract class BaseCosmosDbOptions
-	{
-		public string Uri { get; set; }
-		public string AuthorizationKey { get; set; }
-		public string DatabaseName { get; set; }
-	}
-}
